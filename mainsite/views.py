f5830b5953a9b449ececb0104edb857ab9cbd5e1
s9200801 <k9200801@gmail.com>
2018-10-19 23:58:07 +0800

    first commit

---
+++ mainsite/views.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
+
+# Create your views here.
+
+def homepage(request):
+    template=get_template('index.html')
+    posts=Post.objects.all()
+    now=datetime.now()
+    html=template.render(locals())
+    #post_lists=list()
+    #for count,post in enumerate(posts):
+    #    post_lists.append("No.{}:".format(str(count))+str(post.title)+"<br>")
+    #    post_lists.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
+    return HttpResponse(html)
+def showpost(request,slug):
+    template=get_template('post.html')
+    try:
+        post=Post.objects.get(slug=slug)
+        if post != None:
+            html=template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
